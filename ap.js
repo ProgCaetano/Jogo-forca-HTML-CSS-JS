0592d8cb31e9836641c8a4ff50d36df3a37043aa
Caetano <122699973+ProgCaetano@users.noreply.github.com>
2024-11-17 14:03:22 -0300

    att

--- ap.js
+++ ap.js
@@ -5,6 +5,88 @@
 const listaPalavras = ['javascript','html','youtube','css'];
 
 let palavraEscolhida;
-let exibicçaoPalavra;
+let exibicaoPalavra;
 let letrasChutadas;
 let tentativasRestates;
+
+
+//FUNCÇÃO PARA INICIAR O JOGO
+
+function iniciarJogo(){
+    //escolher palavra aleatoria dalista
+    palavraEscolhida = listaPalavras[Math.floor(Math.random(listaPalavras.length))]
+
+    //inivializar a exibição 
+    exibicaoPalavra = Array(palavraEscolhida.length).fill('_');
+
+    //iniciaçizar a lista de palavras chutadas
+    letrasChutadas = [];
+
+    //definir o numero maximo de tentativaas
+    tentativasRestates = 7;
+
+    numeroErros = 0;
+
+    atualizarExibicao();
+}
+
+function atualizarExibicao(){
+    document.getElementById('exibicao-palavra').innerText = exibicaoPalavra.join(" ");
+    document.getElementById("letras-chutadas").innerText = `${letrasChutadas.join(", ")}`
+    
+    document.getElementById("imagem").src = `img/forca${numeroErros}.png`
+
+    //verificar se o jogo terminou
+    if(tentativasRestates ===0){
+        encerrarJogo('FIM DE JOGO!!!');
+    } else if(!exibicaoPalavra.includes('_')){
+        encerrarJogo('Parabéns! Você venceu!')
+    }
+
+}
+
+function chutarLetra(){
+    const entradaLetra = document.getElementById('entrada-letra');
+    const letra = entradaLetra.value.toLowarCase;
+
+    if(!letra.match(/[a-zà-ùç]/i)){
+        alert('Por favor, insira uma letra válida.')
+        return;
+    }
+    if(letrasChutadas.includes(letra)){
+        alert('Você já tentou esta letra, Tente outra.')
+        return;
+    }
+
+    letrasChutadas.push(letra);
+
+    if(palavraEscolhida.includes(letra)){
+        for (let i=0; i < palavraEscolhida.length; i++){
+            if (palavraEscolhida[i] === letra){
+                exibicaoPalavra[i] = letra;
+            }
+        }
+    } else {
+        tentativasRestates--;
+        numeroErros++;
+    }
+    entradaLetra.value = '';
+
+    atualizarExibicao();
+}
+
+function encerrarJogo(mensagem){
+    //desanilitar campinho
+    document.getElementById('entrada-letra').disabled = true;
+
+    //exibir mensagem
+    document.getElementById('mensagem').innerText = mensagem;
+
+    //exibir botao reiniciar
+    document.getElementById('botao-reiniciar').style.display = 'block'
+
+}
+
+
+
+window.load = iniciarJogo();
