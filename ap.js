bb74e0a45a1d0c7272df56b8e9b8e83677f1aa4a
Caetano <122699973+ProgCaetano@users.noreply.github.com>
2024-11-17 12:10:54 -0300

    Adicionando estilo e interação com js

---
+++ ap.js
@@ -0,0 +1,10 @@
+
+
+
+// Adicionano lista de palavras para o jogo
+const listaPalavras = ['javascript','html','youtube','css'];
+
+let palavraEscolhida;
+let exibicçaoPalavra;
+let letrasChutadas;
+let tentativasRestates;
